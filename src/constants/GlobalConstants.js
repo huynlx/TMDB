97fc3f0bb152ded7321ee8603f30648207aa4ca5
Lê Xuân Huynh <huynhlx62@wru.vn>
2022-07-10 00:35:38 +0700

    chore: update source url

--- src/constants/GlobalConstants.js
+++ src/constants/GlobalConstants.js
@@ -1,6 +1,6 @@
 export const FILE_SIZE = 1048576; //1MB
 export const TMDB_IMAGE = "https://image.tmdb.org/t/p/";
-export const embedMovie = (id) => `https://www.2embed.ru/embed/tmdb/movie?id=${id}`;
-export const embedEpisode = (id, season, episode) => `https://www.2embed.ru/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
+export const embedMovie = (id) => `https://2embed.org/embed/${id}`;
+export const embedEpisode = (id, season, episode) => `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
 export const imageResize = (src, dimension = "w200") => `${TMDB_IMAGE}${dimension}${src}`;
 export const imageOriginal = (src) => `${TMDB_IMAGE}original${src}`;
